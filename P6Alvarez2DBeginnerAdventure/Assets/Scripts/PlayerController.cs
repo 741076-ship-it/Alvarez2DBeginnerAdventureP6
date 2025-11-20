affe2d1204bcc6b45426d896ede1cf5a8128e22f
741076-ship-it <741076@student.dusd.net>
2025-11-20 15:42:00 -0800

    Project
    Added project into the repo

---
+++ P6Alvarez2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerControllerTutorialUpdates : MonoBehaviour
+{
+
+    public InputAction LeftAction;
+    public InputAction MoveAction;
+    public float speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        LeftAction.Enable();
+        MoveAction.Enable();
+      
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+       Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+}
